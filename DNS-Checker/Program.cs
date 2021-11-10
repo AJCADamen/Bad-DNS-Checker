dc8fac9c44f397530fafbb86aa2c715008a9fb4d
AJCADamen <andreasdamen@gmail.com>
2021-11-10 11:38:01 +0100

    mail update

--- DNS-Checker/Program.cs
+++ DNS-Checker/Program.cs
@@ -13,47 +13,69 @@ namespace DNS_Checker
 
             if (File.ReadAllText("latestdomains.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("Malware.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("malware-nl.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("notrack-malware.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("phishing_army_blocklist_extended.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("phishing-nl.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("Prigent-Phishing.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("ransomware-nl.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("urlhaus.abuse.ch.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (File.ReadAllText("urlhaus-filter-domains.txt").Contains(s))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("There is a match");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("No match found");
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
