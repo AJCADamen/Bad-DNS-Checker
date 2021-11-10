04a82d017e66fdca4d8977206636fef1a2569e06
AJCADamen <andreasdamen@gmail.com>
2021-11-10 11:34:53 +0100

    mvp

---
+++ DNS-Checker/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace DNS_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert DNS record");
+            string s = Console.ReadLine();
+            bool found = false;
+
+            if (File.ReadAllText("latestdomains.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("Malware.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("malware-nl.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("notrack-malware.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("phishing_army_blocklist_extended.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("phishing-nl.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("Prigent-Phishing.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("ransomware-nl.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("urlhaus.abuse.ch.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else if (File.ReadAllText("urlhaus-filter-domains.txt").Contains(s))
+            {
+                Console.WriteLine("There is a match");
+            }
+            else
+            {
+                Console.WriteLine("No match found");
+            }
+        }
+    }
+}
